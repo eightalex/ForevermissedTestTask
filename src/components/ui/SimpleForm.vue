9b78824c2e33596c989c0b33cd19c6cb1da22279
eightalex <eight.alex@gmail.com>
2023-03-03 00:17:34 +0200

    fix(simpleForm): clear input

--- src/components/ui/SimpleForm.vue
+++ src/components/ui/SimpleForm.vue
@@ -1,20 +1,52 @@
 <script setup>
+import { ref, nextTick } from 'vue';
 import InputCustom from '@/components/ui/InputCustom.vue';
 import ButtonCustom from '@/components/ui/ButtonCustom.vue';
 
-defineProps({
+const props = defineProps({
     placeholder: {
         type: String,
         default: '',
     },
+    buttonText: {
+        type: String,
+        default: 'Add',
+    },
+    hasError: {
+        type: Boolean,
+        default: false,
+    },
 });
+
+const value = ref('');
+const emit = defineEmits(['send']);
+
+const send = async () => {
+    emit('send', value.value);
+
+    await nextTick();
+
+    if (value.value && !props.hasError) {
+        value.value = '';
+    }
+};
 </script>
 
 <template>
     <div class="simple-form">
-        <InputCustom class="simple-form__input" type="email" :placeholder="placeholder"/>
-        <ButtonCustom class="simple-form__btn" type="light">
-            <slot></slot>
+        <InputCustom
+            class="simple-form__input"
+            type="email"
+            v-model="value"
+            :hasError="hasError"
+            :placeholder="placeholder"
+        />
+        <ButtonCustom
+            class="simple-form__btn"
+            type="light"
+            @click="send"
+        >
+            {{ buttonText }}
         </ButtonCustom>
     </div>
 </template>
